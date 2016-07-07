1b3abc7d28c36fe2d0ee2d5f1f32873822c56e94
CapsE <larskrafft@yahoo.de>
2016-07-07 14:44:25 +0200

    Added Testpage

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,22 @@ module.exports = function(express,HTMLSync){
   router.get("/", function(req, res){
     res.render('index', {});
   });
+  
+  router.get("/test", function(req, res){
+    var hPerlin = new PerlinGenerator();
+    var img = {x: 200, y: 200, string: ""};
+
+    for(var x = 0; x < img.x; x++){
+      for(var y = 0; y < img.y; y++){
+        var string = parseInt(hPerlin.noise(x/25,y/25) * 255).toString(16);
+        while(string.length < 2){
+          string = "0" + string;
+        }
+        img.string += string;
+      }
+    }
+    res.render('test', {img: JSON.stringify(img)});
+  });
 
   return router;
 };
